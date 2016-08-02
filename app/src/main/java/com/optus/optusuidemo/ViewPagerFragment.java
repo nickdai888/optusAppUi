e1e8a92b33fb3651a03e61a09400ba4d032548eb
nickdai888 <nickdai888@gmail..com>
2016-08-03 01:04:53 +1000

    modify some layout para

--- app/src/main/java/com/optus/optusuidemo/ViewPagerFragment.java
+++ app/src/main/java/com/optus/optusuidemo/ViewPagerFragment.java
@@ -2,12 +2,8 @@ package com.optus.optusuidemo;
 
 import android.content.Context;
 import android.os.Bundle;
-import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
-import android.util.Log;
-import android.view.Gravity;
 import android.view.LayoutInflater;
-import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
